734ca0f1d12dfd569d5b38d23aa1b3fed88267bb
bmareddy <madhumohan.m@gmail.com>
2020-01-08 21:44:43 -0800

    weighted mean sooo easy

---
+++ 0_1_weighted_mean.py
@@ -0,0 +1,16 @@
+x=list(map(int,list(input().split(' '))))
+w = input()
+if w:
+    w=list(map(int,list(w.split(' '))))
+
+def average(x,weights=None):
+    """
+    x is array of integers
+    w is array of weights corresponding to the integers
+    returns weighted average if weights are provided. else returns simple average
+    """
+    if weights:
+        return sum(i*j for i,j in zip(x,weights)) / sum(weights)
+    return sum(x) / len(x)
+wm = average(x,weights=w)
+print(f"{wm:.1f}")
